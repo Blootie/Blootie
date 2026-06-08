05667b87a1df464e82030b572c8cc33fc918bed8
Blootie <mariana.g.c.216@gmail.com>
2026-06-08 14:36:40 -0500

    Update models.py

--- blootie_app/models.py
+++ blootie_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# --- PERFIL DE USUARIO ---
 class Perfil(models.Model):
     ROL_CHOICES = [
         ('admin', 'Administrador'),
