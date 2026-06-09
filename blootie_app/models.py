7f92e4113aca833a3155af9e8c43f8ceb5b8aa04
Blootie <mariana.g.c.216@gmail.com>
2026-06-09 00:53:22 -0500

    Update models.py

--- blootie_app/models.py
+++ blootie_app/models.py
@@ -85,6 +85,7 @@ class Logro(models.Model):
 
 class ConfiguracionActividad(models.Model):
     nombre = models.CharField(max_length=100)
+    visible = models.BooleanField(default=True)
     slug = models.SlugField(unique=True)
     esta_activa = models.BooleanField(default=True)
     
