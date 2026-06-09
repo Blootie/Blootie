cca2e1ad4def7b7032b3ac35bcb5ffd5b6d06c0c
Blootie <mariana.g.c.216@gmail.com>
2026-06-09 02:11:24 -0500

    Update urls.py

--- blootie_app/urls.py
+++ blootie_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('redirigir/', views.redirect_by_role, name='redirect_by_role'),
+    path('register/', views.register_view, name='register'),
 
     # Admin
     path('admin-panel/', views.admin_dashboard, name='admin_dashboard'),
