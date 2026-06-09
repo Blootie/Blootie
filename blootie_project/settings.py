4b77243b00a7b04898241ee1f917e4bc7919fecf
Blootie <mariana.g.c.216@gmail.com>
2026-06-09 02:22:51 -0500

    Update settings.py

--- blootie_project/settings.py
+++ blootie_project/settings.py
@@ -65,8 +65,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/redirigir/'
