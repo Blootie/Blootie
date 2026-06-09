ec28af88dda68f48bfda7418ff3d4963bd7fa29d
Blootie <mariana.g.c.216@gmail.com>
2026-06-09 00:58:54 -0500

    Update and rename 0004_configuracionactividad.py to 0004_vacio.py

---
+++ blootie_app/migrations/0004_vacio.py
@@ -0,0 +1,5 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [('blootie_app', '0003_logro_icono_progreso_actividad_ref_and_more')]
+    operations = []
